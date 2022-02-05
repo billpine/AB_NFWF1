45d100d100fce35010fa579cc8e33325d762d15e
Pine <billpine@ufl.edu>
2022-02-05 18:52:46 -0500

    corrected proportion calculator

--- proportion calculator.R
+++ proportion calculator.R
@@ -1,7 +1,6 @@
 
-library(readxl)
-library(tidyverse)
-library(dplyr)
+#this calculates the proportions of the size classes 
+#from te FWC data
 
 library(readxl)
 library(tidyverse)
@@ -116,106 +115,93 @@ proportion_spat_p9<-num_spat_p9/num_total_p9
 hist(d2$SH)
 
 ###SEED
-num_seed<-length(h2$SH[h2$SH>=26 & h2$SH<75])
 
-
-p2 <- subset(h3, h3$Period == 2)
-num_seed_p2<-length(p2$SH[p2$SH>=26 & p2$SH<76])
+num_seed_p2<-length(subset(p2$SH, p2$SH>=26 & p2$SH<76))
 num_total_p2<-length(p2$SH)
 proportion_seed_p2<-num_seed_p2/num_total_p2
 #0.23 seed
 
-p3 <- subset(h3, h3$Period == 3)
-num_seed_p3<-length(p3$SH[p3$SH>=26 & p3$SH<76])
+
+num_seed_p3<-length(subset(p3$SH, p3$SH>=26 & p3$SH<76))
 num_total_p3<-length(p3$SH)
 proportion_seed_p3<-num_seed_p3/num_total_p3
-#0.61 seed
+#0.60 seed
 
-p4 <- subset(h3, h3$Period == 4)
-num_seed_p4<-length(p4$SH[p4$SH>=26 & p4$SH<76])
+num_seed_p4<-length(subset(p4$SH, p4$SH>=26 & p4$SH<76))
 num_total_p4<-length(p4$SH)
 proportion_seed_p4<-num_seed_p4/num_total_p4
 #0.10 seed
 
-p5 <- subset(h3, h3$Period == 5)
-num_seed_p5<-length(p5$SH[p5$SH>=26 & p5$SH<76])
+num_seed_p5<-length(subset(p5$SH, p5$SH>=26 & p5$SH<76))
 num_total_p5<-length(p5$SH)
 proportion_seed_p5<-num_seed_p5/num_total_p5
-#0.19 seed
+#0.16 seed
 
-p6 <- subset(h3, h3$Period == 6)
-num_seed_p6<-length(p6$SH[p6$SH>=26 & p6$SH<76])
+num_seed_p6<-length(subset(p6$SH, p6$SH>=26 & p6$SH<76))
 num_total_p6<-length(p6$SH)
 proportion_seed_p6<-num_seed_p6/num_total_p6
 #0.02 seed
 
-p7 <- subset(h3, h3$Period == 7)
-num_seed_p7<-length(p7$SH[p7$SH>=26 & p7$SH<76])
+num_seed_p7<-length(subset(p7$SH, p7$SH>=26 & p7$SH<76))
 num_total_p7<-length(p7$SH)
 proportion_seed_p7<-num_seed_p7/num_total_p7
 #0.07 seed
 
-p8 <- subset(h3, h3$Period == 8)
-num_seed_p8<-length(p8$SH[p8$SH>=26 & p8$SH<76])
+num_seed_p8<-length(subset(p8$SH, p8$SH>=26 & p8$SH<76))
 num_total_p8<-length(p8$SH)
 proportion_seed_p8<-num_seed_p8/num_total_p8
 #0.11 seed
 
-p9 <- subset(h3, h3$Period == 9)
-num_seed_p9<-length(p9$SH[p9$SH>=26 & p9$SH<76])
+num_seed_p9<- length(subset(p9$SH, p9$SH>=26 & p9$SH<76))
 num_total_p9<-length(p9$SH)
 proportion_seed_p9<-num_seed_p9/num_total_p9
-#0.31 seed
-
-hist(d2$SH)
-
-
+#0.01 seed
 
 ####Legal######
 p2 <- subset(h3, h3$Period == 2)
-num_Legal_p2<-length(p2$SH[p2$SH>=76])
+num_Legal_p2<-length(subset(p2$SH, p2$SH>=76))
 num_total_p2<-length(p2$SH)
 proportion_Legal_p2<-num_Legal_p2/num_total_p2
-#0.02 Legal
+#0.0002 Legal
 
 p3 <- subset(h3, h3$Period == 3)
-num_Legal_p3<-length(p3$SH[p3$SH>=76])
+num_Legal_p3<-length(subset(p3$SH, p3$SH>=76))
 num_total_p3<-length(p3$SH)
 proportion_Legal_p3<-num_Legal_p3/num_total_p3
-#0.04 Legal
-
-p4 <- subset(h3, h3$Period == 4)
-num_Legal_p4<-length(p4$SH[p4$SH>=76])
-num_total_p4<-length(p4$SH)
-proportion_Legal_p4<-num_Legal_p4/num_total_p4
 #0.03 Legal
 
+p4 <- subset(h3, h3$Period == 4)
+num_Legal_p4<-length(subset(p2$SH, p2$SH>=76))
+num_total_p4<-length(p4$SH)
+proportion_Legal_p4<-num_Legal_p4/num_total_p4
+#0.0002 Legal
+
 p5 <- subset(h3, h3$Period == 5)
-num_Legal_p5<-length(p5$SH[p5$SH>=76])
+num_Legal_p5<-length(subset(p5$SH, p5$SH>=76))
 num_total_p5<-length(p5$SH)
 proportion_Legal_p5<-num_Legal_p5/num_total_p5
-#0.05 Legal
+#0.03 Legal
 
 p6 <- subset(h3, h3$Period == 6)
-num_Legal_p6<-length(p6$SH[p6$SH>=76])
+num_Legal_p6<-length(subset(p6$SH, p6$SH>=76))
 num_total_p6<-length(p6$SH)
 proportion_Legal_p6<-num_Legal_p6/num_total_p6
-#0.02 Legal
+#0.0002 Legal
 
 p7 <- subset(h3, h3$Period == 7)
-num_Legal_p7<-length(p7$SH[p7$SH>=76])
+num_Legal_p7<-length(subset(p2$SH, p2$SH>=76))
 num_total_p7<-length(p7$SH)
 proportion_Legal_p7<-num_Legal_p7/num_total_p7
-#0.06 Legal
+#0.0007 Legal
 
 p8 <- subset(h3, h3$Period == 8)
-num_Legal_p8<-length(p8$SH[p8$SH>=76])
+num_Legal_p8<-length(subset(p8$SH, p8$SH>=76))
 num_total_p8<-length(p8$SH)
 proportion_Legal_p8<-num_Legal_p8/num_total_p8
-#0.11 Legal
+#0 Legal
 
 p9 <- subset(h3, h3$Period == 9)
-num_Legal_p9<-length(p9$SH[p9$SH>=76])
+num_Legal_p9<-length(subset(p9$SH, p9$SH>=76))
 num_total_p9<-length(p9$SH)
 proportion_Legal_p9<-num_Legal_p9/num_total_p9
 #0.30 Legal
